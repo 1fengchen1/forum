9a5b2aef5f26a5e4492f64f1bcf6fd17d8f1e232
0fengchegn0 <758896823@qq.com>
2017-09-13 15:35:12 +0800

    '美化admin'

--- blocks/models.py
+++ blocks/models.py
@@ -1,6 +1,17 @@
-from django.db import models
+from django.db import models    #Django规定使用数据库必须要导入的
+
 
 class Block(models.Model):
-    name = models.CharField("模板标题", max_length=100)
-    desc = models.CharField("模板描述", max_length=100)
+    '''定义表结构，就是对类进行定义属性'''
+    name = models.CharField("板块标题", max_length=100)  #设置列类型，名字，长度
+    desc = models.CharField("板块描述", max_length=100)
     manager_name = models.CharField("管理员名字", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "首页"     #在数据表内，添加板块的展示
+        verbose_name_plural = "首页板块"  #数据表名展示
+
+
